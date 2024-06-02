e5d5e6834d3e13eb64ed057dd16f902da97acc57
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2024-06-03 00:27:09 +0530

    Frontend UI design

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -8,7 +8,7 @@ export default function MainPage() {
     const[amountInSourceCurrency,setAmountInSourceCurrency] = useState(0);
     const[amountInTargetCurrency,setAmountInTargetCurrency] = useState(0);
 
-    const handleSubmit = (e=>{
+    const handleSubmit = (e)=>{
 
         e.preventDefault();
         console.log(
@@ -75,7 +75,7 @@ export default function MainPage() {
                 className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-green-800 dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
                 <span 
                     className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Get the target Currency
+                Get the target currency
                 </span>
             </button>    
         </div>
